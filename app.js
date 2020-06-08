3c3c39736eeba588a6b9d9e30507b717d06fb179
Angelo Ibarrola <vino_1105@hotmail.com>
2020-06-08 13:59:57 -0700

    added partials

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res){
+    res.render("home")
+})
+
+app.get("*", function(req,res){
+    res.send("Invalid URL");
+});
+
+
+app.listen(3000,function(){
+    console.log("Server started...");
+});
