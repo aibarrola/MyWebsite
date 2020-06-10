fe18e5fdcbbd1df46e13b3e43d9cd609413a6f21
Angelo Ibarrola <vino_1105@hotmail.com>
2020-06-10 12:16:25 -0700

    added about me section

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 var express = require("express");
-
 var app = express();
 
+
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
 
 app.get("/", function(req,res){
-    res.render("home")
-})
+    res.render("projects");
+});
+
+app.get("/projects", function(req,res){
+    res.render("projects");
+});
+
+app.get("/projects/affirmation", function(req,res){
+    res.render("affirmation");
+});
+
+
+
 
 app.get("*", function(req,res){
     res.send("Invalid URL");
 });
-
 
 app.listen(3000,function(){
     console.log("Server started...");
